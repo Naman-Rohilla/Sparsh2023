a4eaf8aa589e6b3a6ba9aacae7e9bb11dfdeee7d
Pradiprajvala <84765833+Pradiprajvala@users.noreply.github.com>
2023-03-04 20:20:15 +0530

    Revert "Adding fetch in ca"

--- src/Components/campusAmbassador/CaRegister.js
+++ src/Components/campusAmbassador/CaRegister.js
@@ -16,21 +16,6 @@ const darkTheme = createTheme({
 const CaRegister = () => {
   const [width, setWidth] = useState(window.innerWidth);
 
-  const [name, setName] = useState(null);
-  const [contact_info, setcontact_info] = useState(null);
-  const [email, setemail] = useState(null);
-  const [institute_name, setinstitute_name] = useState(null);
-  // const [institute_name , setinstitute_name] = useState(null);
-  const [course, setcourse] = useState(null);
-  const [year, setyear] = useState(null);
-  const [LinkedIn, setLinkedIn] = useState(null);
-  const [instagram, setinstagram] = useState(null);
-  const [Facebook, setFacebook] = useState(null);
-  const [College_ID_card, setCollege_ID_card] = useState(null);
-  const [Aadhar_card, setAadhar_card] = useState(null);
-
-
-
   function handleWindowSizeChange() {
     setWidth(window.innerWidth);
   }
@@ -44,32 +29,7 @@ const CaRegister = () => {
   const isMobile = width <= 786;
 
   const submitHandler = () => {
-
-    console.log(name, contact_info, email, institute_name, course, year, LinkedIn, instagram, Facebook, College_ID_card, Aadhar_card);
-
-    fetch('https://sparsh-auth-production.up.railway.app/api/campus_ambassador/register', {
-      method: 'POST',
-      body: JSON.stringify({
-        "name": name,
-      "contact_info": contact_info,
-      "email": email,
-        "institute_name": institute_name,
-        "course": course,
-        "year": year,
-        "linkedIn": LinkedIn,
-        "instagram": instagram,
-        "Facebook": Facebook,
-      "Aadhar_card": Aadhar_card,
-      "College_ID_card": College_ID_card,
-      }),
-      headers: {
-        'Content-type': 'multipart/form-data',
-      },
-    })
-    .then(res => (console.log(res)))
-    .catch(err => (console.log(err)))
-
-
+    // submit the values
   };
   return (
     <ThemeProvider theme={darkTheme}>
@@ -96,28 +56,28 @@ const CaRegister = () => {
             sx={{ width: isMobile ? "80%" : "30%" }}
             spacing={2}
           >
-            <TextField fullWidth id="" label="Name" onChange={(e) => setName(e.target.value)} variant="filled" />
-            <TextField id="" label="Contact Number" onChange={(e) => setcontact_info(e.target.value)} variant="filled" />
-            <TextField id="" label="E-Mail" onChange={(e) => setemail(e.target.value)} variant="filled" />
-            <TextField id="" label="Institute Name" onChange={(e) => setinstitute_name(e.target.value)} variant="filled" />
-            <TextField id="" label="Course" onChange={(e) => setcourse(e.target.value)} variant="filled" />
-            <TextField id="" label="Year of Graduation" onChange={(e) => setyear(e.target.value)} variant="filled" />
+            <TextField fullWidth id="" label="Name" variant="filled" />
+            <TextField id="" label="Contact Number" variant="filled" />
+            <TextField id="" label="E-Mail" variant="filled" />
+            <TextField id="" label="Institute Name" variant="filled" />
+            <TextField id="" label="Course" variant="filled" />
+            <TextField id="" label="Year of Graduation" variant="filled" />
           </Stack>
           <Stack
             direction="column"
             spacing={2}
             sx={{ width: isMobile ? "80%" : "30%" }}
           >
-            <TextField id="" label="LinkedIn ID" onChange={(e) => setLinkedIn(e.target.value)} variant="filled" />
-            <TextField id="" label="Instagram ID" onChange={(e) => setinstagram(e.target.value)} variant="filled" />
-            <TextField id="" label="Facebook ID" onChange={(e) => setFacebook(e.target.value)} variant="filled" />
+            <TextField id="" label="LinkedIn ID" variant="filled" />
+            <TextField id="" label="Instagram ID" variant="filled" />
+            <TextField id="" label="Facebook ID" variant="filled" />
             <Button
               sx={{ bgcolor: "#5C5C5C", color: "white" }}
               variant="contained"
               component="label"
             >
               UPLOAD COLLEGE ID
-              <input type="file" onChange={(e) => setCollege_ID_card(e.target.value)} hidden />
+              <input type="file" hidden />
             </Button>
 
             <Button
@@ -126,12 +86,12 @@ const CaRegister = () => {
               component="label"
             >
               UPLOAD AADHAAR
-              <input type="file" onChange={(e) => setAadhar_card(e.target.value)} hidden />
+              <input type="file" hidden />
             </Button>
           </Stack>
         </Stack>
         <Stack direction="row" justifyContent="center">
-          <Button sx={{ fontSize: "2rem", px: "2rem" }} onClick={submitHandler} variant="contained">
+          <Button sx={{ fontSize: "2rem", px: "2rem" }} variant="contained">
             SUBMIT
           </Button>
         </Stack>
