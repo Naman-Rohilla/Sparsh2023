31841dd741bc80bc100301e4d008120b2f47627b
Snehpatel007 <81500980+Snehpatel007@users.noreply.github.com>
2023-03-21 23:33:02 +0530

    Add files via upload

---
+++ src/Components/signin/newsCard.js
@@ -0,0 +1,24 @@
+import Button from 'react-bootstrap/Button';
+import Card from 'react-bootstrap/Card';
+import "./newsCard.css"
+
+const NewsCard=(props)=> {
+  return (
+    <div class="card">
+        <img className='img' src={props.image}/>
+          <div className='content'>
+             <div className='heading'>
+                 {props.heading}
+             </div>
+             <div className='text'>
+                {props.text}
+             </div>
+             <div className='date'>
+                 {props.date}
+             </div>
+          </div>
+        </div>
+  );
+}
+
+export default NewsCard;
